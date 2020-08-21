52278f9a94caca84b7c55842530685c498da1848
Anmol <anmolpratapsingh1997@gmail.com>
2020-08-21 15:32:50 +0530

    Initial commit

---
+++ card.js
@@ -0,0 +1,95 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const boxen = require("boxen");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const clear = require("clear");
+const open = require("open");
+
+clear();
+
+const prompt = inquirer.createPromptModule();
+
+const questions = [
+    {
+        type: "list",
+        name: "action",
+        message: "What you want to do?",
+        choices: [
+            {
+                name: `Send me an ${chalk.green.bold("email")}?`,
+                value: () => {
+                    open("mailto:anmolpratapsingh1997@gmail.com");
+                    console.log("\nDone, see you soon.\n");
+                }
+            },
+            {
+                name: "Just quit.",
+                value: () => {
+                    console.log("Hasta la vista.\n");
+                }
+            }
+        ]
+    }
+];
+
+const data = {
+    name: chalk.bold.green("                  Anmol Pratap Singh"),
+    handle: chalk.white("@anmol098"),
+    work: `${chalk.white("Software Engineer at")} ${chalk
+        .hex("#2b82b2")
+        .bold("ClearTax")}`,
+    twitter: chalk.gray("https://twitter.com/") + chalk.cyan("misteranmol"),
+    github: chalk.gray("https://github.com/") + chalk.green("anmol098"),
+    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("anmol098"),
+    web: chalk.cyan("https://anmolsingh.me"),
+    npx: chalk.red("npx") + " " + chalk.white("anmol"),
+
+    labelWork: chalk.white.bold("       Work:"),
+    labelTwitter: chalk.white.bold("    Twitter:"),
+    labelGitHub: chalk.white.bold("     GitHub:"),
+    labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+    labelWeb: chalk.white.bold("        Web:"),
+    labelCard: chalk.white.bold("       Card:")
+};
+
+const me = boxen(
+    [
+        `${data.name}`,
+        ``,
+        `${data.labelWork}  ${data.work}`,
+        ``,
+        `${data.labelTwitter}  ${data.twitter}`,
+        `${data.labelGitHub}  ${data.github}`,
+        `${data.labelLinkedIn}  ${data.linkedin}`,
+        `${data.labelWeb}  ${data.web}`,
+        ``,
+        `${data.labelCard}  ${data.npx}`,
+        ``,
+        `${chalk.italic(
+            "There are two ways to write error-free programs;"
+        )}`,
+        `${chalk.italic("only the third one works.")}`
+    ].join("\n"),
+    {
+        margin: 1,
+        float: 'center',
+        padding: 1,
+        borderStyle: "single",
+        borderColor: "green"
+    }
+);
+
+console.log(me);
+const tip = [
+    `Tip: Try ${chalk.cyanBright.bold(
+        "cmd/ctrl + click"
+    )} on the links above`,
+    '',
+].join("\n");
+console.log(tip);
+
+
+prompt(questions).then(answer => answer.action());
